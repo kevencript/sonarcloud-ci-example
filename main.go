5d641d221cdf2597cea638ac37dfb33d501ba729
kevencript <kevencript@gmail.com>
2023-03-15 02:25:00 -0300

    feat: :sparkles: Added Go example HTTP server with Tests

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	// Here we are just printing a random Hash in order to verify 
+	// on Go tests if everything is working fine. This way, we can
+	// implement this into the Sonarqube analysis (This is an EXAMPLE)
+	http.HandleFunc("/first", handlerSecondHash)
+
+
+	http.HandleFunc("/second", handlerFirstHash)
+
+	http.ListenAndServe(":8080", nil)
+}
+
+func getFirstHash() string {
+	return "8743b52063cd84097a65d1633f5c74f5"
+}
+
+func getSecondHash() string {
+	return "0cc175b9c0f1b6a831c399e269772661"
+}
+
+func handlerFirstHash(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, getFirstHash())
+}
+
+func handlerSecondHash(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, getSecondHash())
+}
+
---
+++ main_test.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestHandlerFirstHash(t *testing.T) {
+	req, err := http.NewRequest("GET", "/first", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rec := httptest.NewRecorder()
+	handler := http.HandlerFunc(handlerFirstHash)
+
+	handler.ServeHTTP(rec, req)
+
+	if status := rec.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	expected := getFirstHash()
+	if rec.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rec.Body.String(), expected)
+	}
+}
+
+func TestHandlerSecondHash(t *testing.T) {
+	req, err := http.NewRequest("GET", "/second", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rec := httptest.NewRecorder()
+	handler := http.HandlerFunc(handlerSecondHash)
+
+	handler.ServeHTTP(rec, req)
+
+	if status := rec.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	expected := getSecondHash()
+	if rec.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rec.Body.String(), expected)
+	}
+}
+
+func TestGetSecondHash(t *testing.T) {
+	expected := "0cc175b9c0f1b6a831c399e269772661"
+	result := getSecondHash()
+	if result != expected {
+		t.Errorf("getSecondHash() failed, expected %s but got %s", expected, result)
+	}
+}
+
+func TestGeFirstHash(t *testing.T) {
+	expected := "8743b52063cd84097a65d1633f5c74f5"
+	result := getFirstHash()
+	if result != expected {
+		t.Errorf("getFirstHash() failed, expected %s but got %s", expected, result)
+	}
+}
+
+
